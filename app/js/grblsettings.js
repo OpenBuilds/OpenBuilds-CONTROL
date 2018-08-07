a1e92dcdb77590f07f1956880979b748677bf50e
openbuilds-engineer <p.walt@openbuilds.com>
2018-08-07 17:40:04 +0200

    v 1.0.48 Steps per mm calcs

--- app/js/grblsettings.js
+++ app/js/grblsettings.js
@@ -63,7 +63,7 @@ function grblPopulate() {
               <th class="sortable-column" data-format="number"></th>
               <th class="sortable-column">Parameter</th>
               <th>Value</th>
-
+              <td>calc</th>
           </tr>
         </thead>
 
@@ -84,6 +84,7 @@ function grblPopulate() {
         <option value="7">[7] X:&#9899; Y:&#9899; Z:&#9899;</option>
         </select>
         </td>
+        <td></td>
         </tr>
         <tr><td>$</td><td>3</td><td>Step direction invert</td><td><select id="val-` + 3 + `-input" value="` + grblParams['$3'] + `">
         <option value="0">[0] X:&#9898; Y:&#9898; Z:&#9898;</option>
@@ -94,7 +95,7 @@ function grblPopulate() {
         <option value="5">[5] X:&#9899; Y:&#9898; Z:&#9899;</option>
         <option value="6">[6] X:&#9898; Y:&#9899; Z:&#9899;</option>
         <option value="7">[7] X:&#9899; Y:&#9899; Z:&#9899;</option>
-        </select></td></tr>
+        </select></td><td></td></tr>
         <tr><td>$</td><td>4</td><td>Invert step enable pin</td><td><select id="val-` + 4 + `-input" value="` + grblParams['$4'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td></tr>
         <tr><td>$</td><td>5</td><td>Invert limit pins</td><td><select id="val-` + 5 + `-input" value="` + grblParams['$5'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td></tr>
         <tr><td>$</td><td>6</td><td>Invert probe pin</td><td><select id="val-` + 6 + `-input" value="` + grblParams['$6'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td></tr>
@@ -102,9 +103,9 @@ function grblPopulate() {
           <option value="0">[0] WPos:&#9899; MPos:&#9898; Buf:&#9898;</option>
           <option value="1">[1] WPos:&#9898; MPos:&#9899; Buf:&#9898;</option>
           <option value="2">[2] WPos:&#9899; MPos:&#9898; Buf:&#9899;</option>
-        </select></td></tr>
-        <tr><td>$</td><td>11</td><td>Junction deviation, millimeters</td><td><input type="text" value="` + grblParams['$11'] + `" id="val-` + 11 + `-input"></td></tr>
-        <tr><td>$</td><td>12</td><td>Arc tolerance, millimeters</td><td><input type="text" value="` + grblParams['$12'] + `" id="val-` + 12 + `-input"></td></tr>
+        </select></td><td></td></tr>
+        <tr><td>$</td><td>11</td><td>Junction deviation, millimeters</td><td><input type="text" value="` + grblParams['$11'] + `" id="val-` + 11 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>12</td><td>Arc tolerance, millimeters</td><td><input type="text" value="` + grblParams['$12'] + `" id="val-` + 12 + `-input"></td><td></td></tr>
         <tr><td>$</td><td>13</td><td>Report in inches</td><td><select id="val-` + 13 + `-input" value="` + grblParams['$13'] + `"><option value="0">&#9898; Disable</option><option value="1">&#9899; Enable</option></select></td></tr>
 
         <tr><td>$</td><td>20</td><td>Soft limits enable <small>(Enable and Save Homing first before enabling)<small></td><td><select id="val-` + 20 + `-input" value="` + grblParams['$20'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td></tr>
@@ -119,31 +120,31 @@ function grblPopulate() {
           <option value="5">[5] X:&#9899; Y:&#9898; Z:&#9899;</option>
           <option value="6">[6] X:&#9898; Y:&#9899; Z:&#9899;</option>
           <option value="7">[7] X:&#9899; Y:&#9899; Z:&#9899;</option>
-        </select></td></tr>
-        <tr><td>$</td><td>24</td><td>Homing locate feed rate, mm/min</td><td><input type="text" value="` + grblParams['$24'] + `" id="val-` + 24 + `-input"></td></tr>
-        <tr><td>$</td><td>25</td><td>Homing search seek rate, mm/min</td><td><input type="text" value="` + grblParams['$25'] + `" id="val-` + 25 + `-input"></td></tr>
-        <tr><td>$</td><td>26</td><td>Homing switch debounce delay, milliseconds</td><td><input type="text" value="` + grblParams['$26'] + `" id="val-` + 26 + `-input"></td></tr>
-        <tr><td>$</td><td>27</td><td>Homing switch pull-off distance, millimeters</td><td><input type="text" value="` + grblParams['$27'] + `" id="val-` + 27 + `-input"></td></tr>
+        </select></td><td></td></tr>
+        <tr><td>$</td><td>24</td><td>Homing locate feed rate, mm/min</td><td><input type="text" value="` + grblParams['$24'] + `" id="val-` + 24 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>25</td><td>Homing search seek rate, mm/min</td><td><input type="text" value="` + grblParams['$25'] + `" id="val-` + 25 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>26</td><td>Homing switch debounce delay, milliseconds</td><td><input type="text" value="` + grblParams['$26'] + `" id="val-` + 26 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>27</td><td>Homing switch pull-off distance, millimeters</td><td><input type="text" value="` + grblParams['$27'] + `" id="val-` + 27 + `-input"></td><td></td></tr>
 
-        <tr><td>$</td><td>30</td><td>Maximum spindle speed, RPM</td><td><input type="text" value="` + grblParams['$30'] + `" id="val-` + 30 + `-input"></td></tr>
-        <tr><td>$</td><td>31</td><td>Minimum spindle speed, RPM</td><td><input type="text" value="` + grblParams['$31'] + `" id="val-` + 31 + `-input"></td></tr>
-        <tr><td>$</td><td>32</td><td>Laser-mode enable</td><td><select id="val-` + 32 + `-input" value="` + grblParams['$32'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td></tr>
+        <tr><td>$</td><td>30</td><td>Maximum spindle speed, RPM</td><td><input type="text" value="` + grblParams['$30'] + `" id="val-` + 30 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>31</td><td>Minimum spindle speed, RPM</td><td><input type="text" value="` + grblParams['$31'] + `" id="val-` + 31 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>32</td><td>Laser-mode enable</td><td><select id="val-` + 32 + `-input" value="` + grblParams['$32'] + `"><option value="0">&#x2717; Disable</option><option value="1">&#x2713; Enable</option></select></td><td></td></tr>
 
-        <tr><td>$</td><td>100</td><td>X-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$100'] + `" id="val-` + 100 + `-input"></td></tr>
-        <tr><td>$</td><td>101</td><td>Y-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$101'] + `" id="val-` + 101 + `-input"></td></tr>
-        <tr><td>$</td><td>102</td><td>Z-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$102'] + `" id="val-` + 102 + `-input"></td></tr>
+        <tr><td>$</td><td>100</td><td>X-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$100'] + `" id="val-` + 100 + `-input"></td><td><button class="button" type="button" onclick="xstepspermm()"><i class="fas fa-calculator"></i></button></td></tr>
+        <tr><td>$</td><td>101</td><td>Y-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$101'] + `" id="val-` + 101 + `-input"></td><td><button class="button" type="button" onclick="ystepspermm()"><i class="fas fa-calculator"></i></button></td></tr>
+        <tr><td>$</td><td>102</td><td>Z-axis steps per millimeter</td><td><input type="text" value="` + grblParams['$102'] + `" id="val-` + 102 + `-input"></td><td><button class="button" type="button" onclick="zstepspermm()"><i class="fas fa-calculator"></i></button></td></tr>
 
-        <tr><td>$</td><td>110</td><td>X-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$110'] + `" id="val-` + 110 + `-input"></td></tr>
-        <tr><td>$</td><td>111</td><td>Y-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$111'] + `" id="val-` + 111 + `-input"></td></tr>
-        <tr><td>$</td><td>112</td><td>Z-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$112'] + `" id="val-` + 112 + `-input"></td></tr>
+        <tr><td>$</td><td>110</td><td>X-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$110'] + `" id="val-` + 110 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>111</td><td>Y-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$111'] + `" id="val-` + 111 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>112</td><td>Z-axis maximum rate, mm/min</td><td><input type="text" value="` + grblParams['$112'] + `" id="val-` + 112 + `-input"></td><td></td></tr>
 
-        <tr><td>$</td><td>120</td><td>X-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$120'] + `" id="val-` + 120 + `-input"></td></tr>
-        <tr><td>$</td><td>121</td><td>Y-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$121'] + `" id="val-` + 121 + `-input"></td></tr>
-        <tr><td>$</td><td>122</td><td>Z-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$122'] + `" id="val-` + 122 + `-input"></td></td></tr>
+        <tr><td>$</td><td>120</td><td>X-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$120'] + `" id="val-` + 120 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>121</td><td>Y-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$121'] + `" id="val-` + 121 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>122</td><td>Z-axis acceleration, mm/sec<sup>2</sup></td><td><input type="text" value="` + grblParams['$122'] + `" id="val-` + 122 + `-input"></td></td><td></td></tr>
 
-        <tr><td>$</td><td>130</td><td>X-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$130'] + `" id="val-` + 130 + `-input"></td></tr>
-        <tr><td>$</td><td>131</td><td>Y-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$131'] + `" id="val-` + 131 + `-input"></td></tr>
-        <tr><td>$</td><td>132</td><td>Z-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$132'] + `" id="val-` + 132 + `-input"></td></tr>
+        <tr><td>$</td><td>130</td><td>X-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$130'] + `" id="val-` + 130 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>131</td><td>Y-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$131'] + `" id="val-` + 131 + `-input"></td><td></td></tr>
+        <tr><td>$</td><td>132</td><td>Z-axis maximum travel, millimeters</td><td><input type="text" value="` + grblParams['$132'] + `" id="val-` + 132 + `-input"></td><td></td></tr>
 
         </tbody>
         </table>
@@ -167,29 +168,34 @@ function grblPopulate() {
   }, 100);;
 
   $('#grblSettingsTable').on('keyup paste click', 'input, select', function() {
-    var hasChanged = false;
-    for (var key in grblParams) {
-      if (grblParams.hasOwnProperty(key)) {
-        var j = key.substring(1)
-        var newVal = $("#val-" + j + "-input").val();
-        // Only send values that changed
-        if (parseFloat(newVal) != parseFloat(grblParams[key])) {
-          hasChanged = true;
-        }
-      }
-    }
-    if (hasChanged) {
-      $('#grblSettingsBadge').fadeIn('slow');
-      $('#saveBtn').attr('disabled', false).removeClass('disabled');
-      $('#saveBtnIcon').removeClass('fg-gray').addClass('fg-grayBlue');
-    } else {
-      $('#grblSettingsBadge').fadeOut('slow');
-      $('#saveBtn').attr('disabled', true).addClass('disabled');
-      $('#saveBtnIcon').removeClass('fg-grayBlue').addClass('fg-gray');
-    }
+    checkifchanged()
   });
   $('#grblSettingsBadge').hide();
 }
+
+function checkifchanged() {
+  var hasChanged = false;
+  for (var key in grblParams) {
+    if (grblParams.hasOwnProperty(key)) {
+      var j = key.substring(1)
+      var newVal = $("#val-" + j + "-input").val();
+      // Only send values that changed
+      if (parseFloat(newVal) != parseFloat(grblParams[key])) {
+        hasChanged = true;
+      }
+    }
+  }
+  if (hasChanged) {
+    $('#grblSettingsBadge').fadeIn('slow');
+    $('#saveBtn').attr('disabled', false).removeClass('disabled');
+    $('#saveBtnIcon').removeClass('fg-gray').addClass('fg-grayBlue');
+  } else {
+    $('#grblSettingsBadge').fadeOut('slow');
+    $('#saveBtn').attr('disabled', true).addClass('disabled');
+    $('#saveBtnIcon').removeClass('fg-grayBlue').addClass('fg-gray');
+  }
+}
+
 
 function grblSaveSettings() {
   for (var key in grblParams) {
