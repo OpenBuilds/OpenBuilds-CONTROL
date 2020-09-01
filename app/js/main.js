e96156004463eadaebeac9b259811bf7e39cc2a2
openbuilds-engineer <p.walt@openbuilds.com>
2020-09-01 18:16:16 +0200

    v1.0.245

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ function setWindowTitle(status) {
 
   if (status) {
     string += " v" + status.driver.version
-  } else {
+  } else if (laststatus) {
     string += " v" + laststatus.driver.version
   }
 
