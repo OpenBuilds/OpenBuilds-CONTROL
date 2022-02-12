fd67d42356a3b70279e03de774bed97672047723
Bob Wood <rlwoodjr@gmail.com>
2022-02-11 18:48:23 -0600

    Update pi-install.sh
    Change to Basic-SENDER

--- pi-install.sh
+++ pi-install.sh
@@ -1,5 +1,5 @@
 echo "---------------------------------------------------"
-echo "[STARTING]  OPENBUILDS-CONTROL INSTALL SCRIPT:     "
+echo "[STARTING]  Basic-SENDER INSTALL SCRIPT:     "
 echo "            Please wait for each step to complete. "
 echo "---------------------------------------------------"
 echo "(1/10) Updating Repositories..."
@@ -16,19 +16,19 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo "(6/10) Updating npm..."
 sudo npm install -g npm@latest
-echo "(7/10) Downloading OpenBuilds-CONTROL source code..."
-cd ~; git clone https://github.com/OpenBuilds/OpenBuilds-CONTROL.git
-cd ~/OpenBuilds-CONTROL
-echo "(8/10) Installing OpenBuilds-CONTROL dependencies..."
+echo "(7/10) Downloading Basic-SENDER source code..."
+cd ~; git clone https://github.com/rlwoodjr/Basic-SENDER.git
+cd ~/Basic-SENDER
+echo "(8/10) Installing Basic-SENDER dependencies..."
 chmod 777 ~/.config
 npm install
-echo "(9/10) Recompiling OpenBuilds-CONTROL dependencies..."
+echo "(9/10) Recompiling Basic-SENDER dependencies..."
 npm rebuild
 npm install electron-rebuild
-~/OpenBuilds-CONTROL/node_modules/.bin/electron-rebuild
+~/Basic SENDER/node_modules/.bin/electron-rebuild
 echo "(10/10) Creating Menu and Desktop Shortcuts..."
-cp ~/OpenBuilds-CONTROL/pi-shortcut.desktop ~/Desktop/OpenBuilds-CONTROL.desktop
-sudo cp ~/OpenBuilds-CONTROL/pi-shortcut.desktop /usr/share/applications/OpenBuilds-CONTROL.desktop
+cp ~/Basic-SENDER/pi-shortcut.desktop ~/Desktop/Basic-SENDER.desktop
+sudo cp ~/Basic-SENDER/pi-shortcut.desktop /usr/share/applications/Basic-SENDERL.desktop
 echo "---------------------------------------------------"
 echo "[COMPLETE] Install Complete!  Thank you!"
 echo "---------------------------------------------------"
