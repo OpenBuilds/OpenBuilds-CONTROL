d2dd8edcde2e0a32a381b6d9833e209665df911e
unknown <p.walt@openbuilds.com>
2024-04-05 22:58:19 +0200

    v1.0.371

---
+++ scripts/notarize.js
@@ -0,0 +1,24 @@
+// From https://github.com/simonw/til/blob/main/electron/sign-notarize-electron-macos.md
+// Based on https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
+
+const {
+  notarize
+} = require("@electron/notarize")
+
+exports.default = async function notarizing(context) {
+  const {
+    electronPlatformName,
+    appOutDir
+  } = context
+  if (electronPlatformName !== "darwin") return
+
+  const appName = context.packager.appInfo.productFilename
+
+  return await notarize({
+    appBundleId: "app.openbuilds.control",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  })
+}
