e1982e260d76dce9335e19e9e94ef556816116ec
unknown <p.walt@openbuilds.com>
2024-04-08 22:21:58 +0200

    Work on #321

--- signWin.js
+++ signWin.js
@@ -2,6 +2,8 @@ const {
   execSync
 } = require('node:child_process')
 
+console.info(`signing with SignWin`)
+
 exports.default = async configuration => {
   if (!process.env.SM_API_KEY) {
     console.info(`Skip signing because SM_API_KEY and  not configured`)
@@ -12,7 +14,10 @@ exports.default = async configuration => {
     throw new Error(`Path of application is not found`)
   }
 
-  execSync(`smctl sign --keypair-alias="${process.env.SM_KEYPAIR_NAME}" --input "${String(configuration.path)}"`, {
+  //signtool.exe sign /sha1 ${{ secrets.SM_CODE_SIGNING_CERT_SHA1_HASH }} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "D:\a\OpenBuilds-CONTROL\OpenBuilds-CONTROL\dist\*.exe"
+  //signtool.exe verify /v /pa "D:\a\OpenBuilds-CONTROL\OpenBuilds-CONTROL\dist\*.exe"
+
+  execSync(`smctl sign --fingerprint="${process.env.SM_CODE_SIGNING_CERT_SHA1_HASH}" --input "${String(configuration.path)}"`, {
     stdio: 'inherit',
   })
   console.info(`signed with SignWin`)
