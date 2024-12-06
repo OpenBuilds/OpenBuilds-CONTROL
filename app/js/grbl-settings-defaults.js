e0ff6fcb2eda270a331475feafb2f2f25c08f7c0
unknown <p.walt@openbuilds.com>
2024-12-06 20:29:09 +0200

    v1.0.385

--- app/js/grbl-settings-defaults.js
+++ app/js/grbl-settings-defaults.js
@@ -258,8 +258,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -298,8 +298,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -338,8 +338,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -378,8 +378,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -418,8 +418,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -449,7 +449,7 @@ function selectMachine(type) {
       $0: "10", //"Step pulse time, microseconds"
       $1: "255", //Step idle delay, milliseconds
       $2: "0", //Step pulse invert, mask
-      $3: "1", //Step direction invert, mask
+      $3: "2", //Step direction invert, mask
       $4: "1", //Invert step enable pin, boolean
       $5: "0", //Invert limit pins, boolean
       $6: "0", //Invert probe pin, boolean
@@ -458,8 +458,8 @@ function selectMachine(type) {
       $12: "0.002", //Arc tolerance, millimeters
       $13: "0", //Report in inches, boolean
       $20: "0", //Soft limits enable, boolean
-      $21: "0", //Hard limits enable, boolean
-      $22: "0", //Homing cycle enable, boolean
+      $21: "1", //Hard limits enable, boolean
+      $22: "1", //Homing cycle enable, boolean
       $23: "7", //Homing direction invert, mask
       $24: "100.000", //Homing locate feed rate, mm/min
       $25: "1000.000", //Homing search seek rate, mm/min
@@ -474,19 +474,19 @@ function selectMachine(type) {
       $34: "5", //Spindle Off Value for RC Servo
       $35: "5", //Spinde Min Value for RC Servo
       $36: "10", //Spindle max Value for RC Servo
-      $44: "1", // Home X first
-      $45: "2", // Then Home Y
+      $44: "3", // Home X first
+      $45: "0", // Then Home Y
       $100: "57.143", //X-axis steps per millimeter-1/16 step
       $101: "57.143", //Y-axis steps per millimeter-1/16 step
       $102: "57.143", //Z-axis steps per millimeter-1/16 step
-      $110: "5000.000", //X-axis maximum rate, mm/min
-      $111: "5000.000", //Y-axis maximum rate, mm/min
-      $112: "5000.000", //Z-axis maximum rate, mm/min
+      $110: "10000.000", //X-axis maximum rate, mm/min
+      $111: "10000.000", //Y-axis maximum rate, mm/min
+      $112: "10000.000", //Z-axis maximum rate, mm/min
       $120: "500.000", //X-axis acceleration, mm/sec^2
       $121: "500.000", //Y-axis acceleration, mm/sec^2
       $122: "500.000", //Z-axis acceleration, mm/sec^2
-      $130: "863.000", //X-axis maximum travel, millimeters
-      $131: "609.000", //Y-axis maximum travel, millimeters
+      $130: "870.000", //X-axis maximum travel, millimeters
+      $131: "600.000", //Y-axis maximum travel, millimeters
       $132: "50.000", //Z-axis maximum travel, millimeters
     }
     setSelectedToolhead('scribe')
